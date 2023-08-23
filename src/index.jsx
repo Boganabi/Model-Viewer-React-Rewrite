8966ced314f20642127c4eb29be973f0efb6cd92
unknown <logan.ashbaugh0001@gmail.com>
2023-08-23 15:28:02 -0700

    trying adding file extensions to imports

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
-import App from './App';
+import App from './App.jsx';
 
 // createRoot(document.getElementById('root')).render(<App />);
 
