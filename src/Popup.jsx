45053df01f769574447e4d8b03cd9d495386f7b7
unknown <logan.ashbaugh0001@gmail.com>
2023-08-25 14:04:34 -0700

    forgot to add that again

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -32,7 +32,7 @@ export default function PopupMenu(props){
         axios({
             method: 'get',
             // url: 'http://localhost:8000/getall',
-            url: '139.182.76.138:8000/getall',
+            url: 'http://139.182.76.138:8000/getall',
         })
         .then(function (response) {
             // handle success
